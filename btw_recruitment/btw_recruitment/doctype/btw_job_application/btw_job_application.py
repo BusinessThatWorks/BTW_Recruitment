6b74941e48e4ec0c6a746b6d08b1d75042dd4f96
Md Sarim Rashid Khan <sarim.khan@clapgrow.com>
2025-11-21 15:55:32 +0530

    updated the doctypes and fixed the structure

--- btw_recruitment/btw_recruitment/doctype/btw_job_application/btw_job_application.py
+++ btw_recruitment/btw_recruitment/doctype/btw_job_application/btw_job_application.py
@@ -5,33 +5,34 @@ import frappe
 from frappe.model.document import Document
 
 
+
 class BTW_Job_Application(Document):
 	pass
 
 import frappe
 
-def after_insert(self, method=None):
+# def after_insert(self, method=None):
 
-    # Safety check
-    if not self.candidate_id:
-        return
+#     # Safety check
+#     if not self.candidate_id:
+#         return
 
-    try:
-        candidate_doc = frappe.get_doc("BTW_Candidate", self.candidate_id)
+#     try:
+#         candidate_doc = frappe.get_doc("BTW_Candidate", self.candidate_id)
 
-        # Prevent duplicates
-        exists = any(
-            row.job_application_id == self.name
-            for row in candidate_doc.table_nxdk
-        )
-        if exists:
-            return
+#         # Prevent duplicates
+#         exists = any(
+#             row.job_application_id == self.name
+#             for row in candidate_doc.table_nxdk
+#         )
+#         if exists:
+#             return
 
-        # Append child row
-        row = candidate_doc.append("table_nxdk", {})
-        row.job_application_id = self.name  # Other fields auto-fetch
+#         # Append child row
+#         row = candidate_doc.append("table_nxdk", {})
+#         row.job_application_id = self.name  # Other fields auto-fetch
 
-        candidate_doc.save(ignore_permissions=True)
+#         candidate_doc.save(ignore_permissions=True)
 
-    except Exception:
-        frappe.log_error(frappe.get_traceback(), "Auto Add Candidate Job Application Failed")
+#     except Exception:
+#         frappe.log_error(frappe.get_traceback(), "Auto Add Candidate Job Application Failed")
