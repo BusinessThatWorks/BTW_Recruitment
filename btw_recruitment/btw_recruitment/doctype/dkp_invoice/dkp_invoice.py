cef66859f50b1efc76c2dfc57d5356f9bfa37be9
Md Sarim Rashid Khan <sarim.khan@clapgrow.com>
2026-02-20 12:21:21 +0530

    removed interview link from the invoice on deleting the invoice

--- btw_recruitment/btw_recruitment/doctype/dkp_invoice/dkp_invoice.py
+++ btw_recruitment/btw_recruitment/doctype/dkp_invoice/dkp_invoice.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2026, Sarim and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class DKP_Invoice(Document):
-	pass
+      
+	def on_trash(self):
+        # ✅ Interview se invoice link hatao before delete
+		if self.interview_ref:
+				frappe.db.set_value(
+					"DKP_Interview", 
+					self.interview_ref, 
+					"invoice_ref", 
+					None
+				)
